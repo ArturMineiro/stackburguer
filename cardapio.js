16cbbb985053360963a95d332ddb6882ed797dde
Artur Mineiro <105992706+ArturMineiro@users.noreply.github.com>
2025-01-28 21:46:09 -0300

    modalfade

---
+++ cardapio.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const response = await fetch("modal-fade.html"); // Certifique-se de que o caminho está correto
+      if (!response.ok) {
+        throw new Error(`Erro ao carregar o modal: ${response.status}`);
+      }
+      const modalHTML = await response.text();
+  
+      // Insere o conteúdo do modal no DOM
+      document.body.insertAdjacentHTML("beforeend", modalHTML);
+    } catch (error) {
+      console.error("Erro ao carregar os modais:", error);
+    }
+  });
+  
